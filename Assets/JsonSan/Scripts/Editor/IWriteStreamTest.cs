9111aabfccc01b5e23bbab4ba6857232982d25a0
ousttrue <ousttrue@gmail.com>
2017-03-02 11:15:37 +0900

    add IWriteStream

---
+++ Assets/JsonSan/Scripts/Editor/IWriteStreamTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using JsonSan.Serializers;
+using System.Text;
+
+
+public class IWriteStreamTest
+{
+    [Test]
+    public void CharArrayStreamTest()
+    {
+
+        var buffer = new char[3];
+        var stream = new CharArrayStream(buffer);
+
+        stream.Write("abc");
+        Assert.AreEqual("abc".ToCharArray(), buffer);
+
+        Assert.Catch(typeof(ArgumentOutOfRangeException), () => stream.Write("d"));
+    }
+
+    [Test]
+    public void StringBuilderStreamTest()
+    {
+        var sb = new StringBuilder();
+        var stream = new StringBuilderStream(sb);
+
+        stream.Write("abc");
+        Assert.AreEqual("abc", sb.ToString());
+
+        stream.Write("d");
+        Assert.AreEqual("abcd", sb.ToString());
+
+        sb.Length = 0;
+        stream.Write("e");
+        Assert.AreEqual("e", sb.ToString());
+    }
+}
---
+++ Assets/JsonSan/Scripts/Serializers/CharStream.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace JsonSan.Serializers
+{
+    public interface IWriteStream<T>
+    {
+        void Write(IEnumerable<T> src);
+        void Write(string src);
+    }
+
+    static class ArraySegementExtensions
+    {
+        //self[index] = value;
+        public static void Set<T>(this ArraySegment<T> self, int index, T value)
+        {
+            if (index<0 || index >= self.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            self.Array[self.Offset + index] = value;
+        }
+    }
+
+    public class CharArrayStream : IWriteStream<Char>
+    {
+        ArraySegment<Char> m_buffer;
+        int m_pos;
+
+        public CharArrayStream(Char[] buffer) : this(buffer, 0)
+        {
+        }
+        public CharArrayStream(Char[] buffer, int offset) : this(buffer, 0, buffer.Length)
+        {
+        }
+        public CharArrayStream(Char[] buffer, int offset, int count) : this(new ArraySegment<char>(buffer, offset, count))
+        {
+        }
+        public CharArrayStream(ArraySegment<Char> buffer)
+        {
+            m_buffer = buffer;
+        }
+
+        public void Write(IEnumerable<char> src)
+        {
+            foreach (var c in src)
+            {
+                m_buffer.Set(m_pos++, c);
+            }
+        }
+        public void Write(string src)
+        {
+            Write((IEnumerable<char>)src);
+        }
+    }
+
+    public class StringBuilderStream: IWriteStream<Char>
+    {
+        StringBuilder m_sb;
+
+        public StringBuilderStream(StringBuilder sb)
+        {
+            m_sb = sb;
+        }
+
+        public void Write(IEnumerable<char> src)
+        {
+            foreach(var c in src)
+            {
+                m_sb.Append(c);
+            }
+        }
+        public void Write(string src)
+        {
+            m_sb.Append(src);
+        }
+    }
+}

ecaf298128e5ba50995fd0b4d7b7509af797b31e
ousttrue <ousttrue@gmail.com>
2017-03-12 13:03:31 +0900

    remove new LambdaSerializer

--- UnityProject/Assets/ObjectStructure/Scripts/Serialization/EmbededTypeSerializations.cs
+++ UnityProject/Assets/ObjectStructure/Scripts/Serialization/EmbededTypeSerializations.cs
@@ -1,6 +1,5 @@
 ﻿using ObjectStructure.Serialization.Deserializers;
 using ObjectStructure.Serialization.Serializers;
-using System;
 using System.Collections.Generic;
 
 
@@ -13,60 +12,60 @@ namespace ObjectStructure.Serialization
             get
             {
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Boolean>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new BooleanDeserializer()
                     );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<SByte>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new SByteDeserializer()
                     );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Int16>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new Int16Deserializer()
                     );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Int32>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new Int32Deserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Int64>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new Int64Deserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Byte>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new ByteDeserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<UInt16>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new UInt16Deserializer()
                 );
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<UInt32>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new UInt32Deserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<UInt64>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new UInt64Deserializer()
                 );
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Single>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new SingleDeserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<Double>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new DoubleDeserializer()
                 );
 
                 yield return TypeSerialization.Create(
-                    new LambdaSerializer<String>((x, f) => f.Value(x))
+                    (x, f) => f.Value(x)
                     , new StringDeserializer()
                 );
 

17fc5e729423245e2befef2314817dbabfc1dc19
ousttrue <ousttrue@gmail.com>
2017-03-15 02:11:12 +0900

    separate IRPCMethod.cs

---
+++ UnityProject/Assets/Osaru/Scripts/RPC/IRPCMethod.cs
@@ -0,0 +1,22 @@
+﻿using Osaru.Serialization.Serializers;
+using System;
+
+
+namespace Osaru.RPC
+{
+    public interface IRPCContext<T>
+        where T : IParser<T>
+    {
+        RPCRequest<T> Request { get; }
+        //RPCResponse<T> Response { get; }
+        void Success();
+        void Success<R>(R result, SerializerBase<R> s);
+        void Error(Exception ex);
+    }
+
+    public interface IRPCMethod
+    {
+        void Call<T>(IRPCContext<T> f)
+            where T : IParser<T>;
+    }
+}
--- UnityProject/Assets/Osaru/Scripts/RPC/RPCMethod.cs
+++ UnityProject/Assets/Osaru/Scripts/RPC/RPCMethod.cs
@@ -6,22 +6,6 @@ using System;
 
 namespace Osaru.RPC
 {
-    public interface IRPCContext<T>
-        where T: IParser<T>
-    {
-        RPCRequest<T> Request { get; }
-        //RPCResponse<T> Response { get; }
-        void Success();
-        void Success<R>(R result, SerializerBase<R> s);
-        void Error(Exception ex);
-    }
-
-    public interface IRPCMethod
-    {
-        void Call<T>(IRPCContext<T> f)
-            where T : IParser<T>;
-    }
-
     // Action<A0>
     public class RPCAction<A0> : IRPCMethod
     {
